a35af4f0640443072257dafc7cbf74b11c4ce082
Miljana Petrovic <miljanaspasic@hotmail.com>
2020-03-25 16:43:19 +0100

    HomePageTest commit

--- src/tests/EnterStoreTest.java
+++ src/tests/EnterStoreTest.java
@@ -1,5 +1,51 @@
 package tests;
 
-public class EnterStoreTest {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.HomePage;
+
+
+public class EnterStoreTest {
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	@BeforeClass
+	public void setup() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators =  new Properties();
+		locators.load(new FileInputStream("config/project.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void EnterTest() {
+		driver.navigate().to(this.locators.getProperty("storeUrl"));
+		
+		HomePage hp = new HomePage(driver, locators, waiter);
+		hp.clickEnter();
+		
+		SoftAssert sa = new SoftAssert();
+		sa.assertTrue(hp.isEntered());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
 }
--- src/tests/RegistrationTest.java
+++ src/tests/RegistrationTest.java
@@ -1,5 +1,35 @@
 package tests;
 
-public class RegistrationTest {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class RegistrationTest {
+	private WebDriver driver;
+	private Properties locators;
+	private WebDriverWait waiter;
+
+	@BeforeClass
+	public void setup() throws FileNotFoundException, IOException {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.locators =  new Properties();
+		locators.load(new FileInputStream("config/project.properties"));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		this.driver.close();
+	}
 }
